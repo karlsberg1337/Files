f763c6037d04ac860a592aeba47225ca57c8a922
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-08 09:15:29 -0500

    Add files via upload

---
+++ Second file.py
@@ -0,0 +1,14 @@
+﻿a = int(input('Необходимо ввести первое число: '))
+b = int(input('Необходимо ввести второе число: '))
+c = int(input('Необходимо ввести третье число: '))
+
+middle_n = a
+if a == b or b == c or a == c:
+    middle_n = 'Error'
+else:
+    if (b > a and b < c) or (b < a and b > c):
+        middle_n = b
+    elif (c > a and c < b) or (c < a and c > b):
+        middle_n = c
+
+print(f'Middle number - {middle_n}')
