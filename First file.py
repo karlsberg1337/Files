fd141c0393c00f0ad8264735b9a939b436e0a185
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-08 09:07:11 -0500

    Add files via upload

---
+++ First file.py
@@ -0,0 +1,15 @@
+﻿a = int(input('Необходимо ввести первое число: '))
+b = int(input('Необходимо ввести второе число: '))
+c = int(input('Необходимо ввести третье число: '))
+max_n = a
+min_n = a
+if b > a and b > c:
+    max_n = x2
+elif c > a and c > b:
+    max_n = x3
+
+if b < a and b < c:
+    min_n = b
+elif c < a and c < b:
+    min_n = c
+print (f'max number - {max_n}\nmin number - {min_n}')
