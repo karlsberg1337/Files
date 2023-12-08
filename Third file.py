1a6cc5852530ef777e4b17bdc5d7cf26a97d027c
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-08 09:15:45 -0500

    Add files via upload

---
+++ Third file.py
@@ -0,0 +1,9 @@
+﻿a = int(input('Необходимо ввести большее число: '))
+b = int(input('Необходимо ввести меньшее число: '))
+
+remainder = a % b
+
+if remainder == 0:
+    print('The number b is a multiple of a')
+else:
+    print (f'The number b is not a multiple of a\nremainder - {remainder}')
